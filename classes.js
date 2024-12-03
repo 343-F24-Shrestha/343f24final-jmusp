60494fd5a3bbbf09c2ecac532edf00031740557f
Colin Kirkwood <kirkw2cj@dukes.jmu.edu>
2024-12-02 22:17:50 -0500

    Database of Classes

---
+++ classes.js
@@ -0,0 +1,538 @@
+{
+    "classes"; [
+        {
+            "course": "CS 149",
+            "title": "Introduction to Programming",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "9:10AM - 10:00AM",
+                    "instructor": "Adeen Ayub"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "MoWeFr",
+                    "times": "10:20AM - 11:10AM",
+                    "instructor": "Adeen Ayub"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "MoWeFr",
+                    "times": "12:40PM - 1:30PM",
+                    "instructor": "Mona Rizvi"
+                },
+                {
+                    "section": "0004-LEC",
+                    "days": "TuTh",
+                    "times": "8:00AM - 9:15AM",
+                    "instructor": "Alvin Chao"
+                },
+                {
+                    "section": "0005-LEC",
+                    "days": "TuTh",
+                    "times": "9:35AM - 10:50AM",
+                    "instructor": "Chandani Shrestha"
+                },
+                {
+                    "section": "0006-LEC",
+                    "days": "TuTh",
+                    "times": "11:10AM - 12:25PM",
+                    "instructor": "Chandani Shrestha"
+                },
+                {
+                    "section": "OP01-LEC",
+                    "days": "TuTh",
+                    "times": "9:35AM - 10:50AM",
+                    "instructor": "Chandani Shrestha"
+                }
+            ]
+        },
+        {
+            "course": "CS 159",
+            "title": "Advanced Programming",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "8:00AM - 8:50AM",
+                    "instructor": "Dee Weikle"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "MoWeFr",
+                    "times": "9:10AM - 10:00AM",
+                    "instructor": "Dee Weikle"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "MoWeFr",
+                    "times": "9:10AM - 10:00AM",
+                    "instructor": "Alvin Chao"
+                },
+                {
+                    "section": "0004-LEC",
+                    "days": "MoWeFr",
+                    "times": "10:20AM - 11:10AM",
+                    "instructor": "Alvin Chao"
+                },
+                {
+                    "section": "0005-LEC",
+                    "days": "MoWeFr",
+                    "times": "1:50PM - 2:40PM",
+                    "instructor": "Chris Mayfield"
+                },
+                {
+                    "section": "0006-LEC",
+                    "days": "MoWeFr",
+                    "times": "3:00PM - 3:50PM",
+                    "instructor": "Chris Mayfield"
+                },
+                {
+                    "section": "0007-LEC",
+                    "days": "TuTh",
+                    "times": "12:45PM - 2:00PM",
+                    "instructor": "Zhoujun Duan"
+                },
+                {
+                    "section": "0008-LEC",
+                    "days": "TuTh",
+                    "times": "2:20PM - 3:35PM",
+                    "instructor": "Chandani Shrestha"
+                }
+            ]
+        },
+        {
+            "course": "CS 227",
+            "title": "Discrete Structures I",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "8:00AM - 8:50AM",
+                    "instructor": "Bradley McCoy"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "MoWeFr",
+                    "times": "9:10AM - 10:00AM",
+                    "instructor": "Bradley McCoy"
+                }
+            ]
+        },
+        {
+            "course": "CS 240",
+            "title": "Data Structures and Algorithms",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "10:20AM - 11:10AM",
+                    "instructor": "Mona Rizvi"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "MoWeFr",
+                    "times": "11:30AM - 12:20PM",
+                    "instructor": "Mona Rizvi"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "TuTh",
+                    "times": "2:20PM - 3:35PM",
+                    "instructor": "Chris Mayfield"
+                }
+            ]
+        },
+        {
+            "course": "CS 261",
+            "title": "Computer Systems I",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "TuTh",
+                    "times": "8:00AM - 9:15AM",
+                    "instructor": "Mohamed Aboutabl"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "TuTh",
+                    "times": "9:35AM - 10:50AM",
+                    "instructor": "Mohamed Aboutabl"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "TuTh",
+                    "times": "11:10AM - 12:25PM",
+                    "instructor": "Mike Lam"
+                },
+                {
+                    "section": "0004-LEC",
+                    "days": "TuTh",
+                    "times": "12:45PM - 2:00PM",
+                    "instructor": "Mike Lam"
+                }
+            ]
+        },
+        {
+            "course": "CS 327",
+            "title": "Discrete Structures II",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "TuTh",
+                    "times": "9:35AM - 10:50AM",
+                    "instructor": "Kevin Molloy"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "TuTh",
+                    "times": "11:10AM - 12:25PM",
+                    "instructor": "Kevin Molloy"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "TuTh",
+                    "times": "12:45PM - 2:00PM",
+                    "instructor": "Florian Buchholz"
+                },
+                {
+                    "section": "0004-LEC",
+                    "days": "TuTh",
+                    "times": "2:20PM - 3:35PM",
+                    "instructor": "Florian Buchholz"
+                }
+            ]
+        },
+        {
+            "course": "CS 343",
+            "title": "Application Development",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "1:50PM - 2:40PM",
+                    "instructor": "Michael Kirkpatrick"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "TuTh",
+                    "times": "12:45PM - 2:00PM",
+                    "instructor": "Michael Stewart"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "TuTh",
+                    "times": "2:20PM - 3:35PM",
+                    "instructor": "Chris Mayfield"
+                }
+            ]
+        },
+        {
+            "course": "CS 347",
+            "title": "Web Development",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "TuTh",
+                    "times": "8:00AM - 9:15AM",
+                    "instructor": "Michael Stewart"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "TuTh",
+                    "times": "9:35AM - 10:50AM",
+                    "instructor": "Michael Stewart"
+                }
+            ]
+        },
+        {
+            "course": "CS 361",
+            "title": "Computer Systems II",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "9:10AM - 10:00AM",
+                    "instructor": "Michael Kirkpatrick"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "MoWeFr",
+                    "times": "10:20AM - 11:10AM",
+                    "instructor": "Michael Kirkpatrick"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "MoWeFr",
+                    "times": "3:00PM - 3:50PM",
+                    "instructor": "Mona Rizvi"
+                },
+                {
+                    "section": "0004-LEC",
+                    "days": "MoWeFr",
+                    "times": "4:10PM - 5:00PM",
+                    "instructor": "Mona Rizvi"
+                }
+            ]
+        },
+        {
+            "course": "CS 412",
+            "title": "Applied Algorithms",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "9:10AM - 10:00AM",
+                    "instructor": "Nathan Sprague"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "MoWeFr",
+                    "times": "10:20AM - 11:10AM",
+                    "instructor": "Nathan Sprague"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "MoWeFr",
+                    "times": "12:40PM - 1:30PM",
+                    "instructor": "Nathan Sprague"
+                }
+            ]
+        },
+        {
+            "course": "CS 430",
+            "title": "Programming Languages",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "TuTh",
+                    "times": "9:35AM - 10:50AM",
+                    "instructor": "Chris Johnson"
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "TuTh",
+                    "times": "11:10AM - 12:25PM",
+                    "instructor": "Chris Johnson"
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "TuTh",
+                    "times": "12:45PM - 2:00PM",
+                    "instructor": "Chris Johnson"
+                }
+            ]
+        },
+        {
+            "course": "CS 444",
+            "title": "Artificial Intelligence",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWe",
+                    "times": "1:50PM - 3:05PM",
+                    "instructor": "Kevin Molloy"
+                }
+            ]
+        },
+        {
+            "course": "CS 455",
+            "title": "Advanced Computer Networking",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWe",
+                    "times": "1:50PM - 3:05PM",
+                    "instructor": "Mohamed Aboutabl"
+                }
+            ]
+        },
+        {
+            "course": "CS 456",
+            "title": "Computer Architecture",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "1:50PM - 2:40PM",
+                    "instructor": "Dee Weikle"
+                }
+            ]
+        },
+        {
+            "course": "CS 458",
+            "title": "Cyber Defense",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "TuTh",
+                    "times": "3:55PM - 5:10PM",
+                    "instructor": "M Hossain Heydari"
+                }
+            ]
+        },
+        {
+            "course": "CS 480",
+            "title": "Topics in Computer Science",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "MoWeFr",
+                    "times": "10:20AM - 11:10AM",
+                    "instructor": "David Bernstein",
+                },
+                {
+                    "section": "0002-LEC",
+                    "days": "TuTh",
+                    "times": "2:20PM - 3:35PM",
+                    "instructor": "Isaac Wang",
+                },
+                {
+                    "section": "0003-LEC",
+                    "days": "TuTh",
+                    "times": "2:20PM - 3:35PM",
+                    "instructor": "Kevin Molloy",
+                }
+            ]
+        },
+        {
+            "course": "CS 482",
+            "title": "Selected Topics in Information Security",
+            "sections": [
+                {
+                    "section": "0001-LEC",
+                    "days": "TuTh",
+                    "times": "2:20PM - 3:35PM",
+                    "instructor": "Xunhua Wang"
+                }
+            ]
+        },
+        {
+            "course": "CS 497",
+            "title": "Independent Study",
+            "sections": [
+                {
+                    "section": "0001-IND",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Nathan Sprague"
+                },
+                {
+                    "section": "0002-IND",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Nathan Sprague"
+                }
+            ]
+        },
+        {
+            "course": "CS 499A",
+            "title": "Honors",
+            "sections": [
+                {
+                    "section": "0001-THE",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Michael Stewart"
+                }
+            ]
+        },
+        {
+            "course": "CS 499C",
+            "title": "Senior Honors Thesis",
+            "sections": [
+                {
+                    "section": "0002-THE",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Michael Stewart"
+                }
+            ]
+        },
+        {
+            "course": "CS 550",
+            "title": "Operating Systems",
+            "sections": [
+                {
+                    "section": "EX81-LEC",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "M Hossain Heydari"
+                }
+            ]
+        },
+        {
+            "course": "CS 559",
+            "title": "Computer Security",
+            "sections": [
+                {
+                    "section": "EX81-LEC",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Xunhua Wang"
+                }
+            ]
+        },
+        {
+            "course": "CS 627",
+            "title": "Cryptography: Algorithms and Applications",
+            "sections": [
+                {
+                    "section": "EX81-LEC",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Xunhua Wang"
+                }
+            ]
+        },
+        {
+            "course": "CS 633",
+            "title": "Computer Forensics",
+            "sections": [
+                {
+                    "section": "EX81-LEC",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Brett Tjaden"
+                }
+            ]
+        },
+        {
+            "course": "CS 635",
+            "title": "Secure Network Operations",
+            "sections": [
+                {
+                    "section": "EX81-LEC",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "Brett Tjaden"
+                }
+            ]
+        },
+        {
+            "course": "CS 685",
+            "title": "Selected Topics II",
+            "sections": [
+                {
+                    "section": "EX81-LEC",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "To be Announced"
+                }
+            ]
+        },
+        {
+            "course": "CS 700",
+            "title": "Thesis Research",
+            "sections": [
+                {
+                    "section": "EX81-THE",
+                    "days": "TBA",
+                    "times": "TBA",
+                    "instructor": "To be Announced"
+                }
+            ]
+        }
+    ]
+}
